dd867b7cd6727c03fa69f7ece2cbcfe44465c12c
Ismam Ilahi <ismamilahi123@gmail.com>
2024-06-23 14:09:03 +0600

    Update earn.blade.php
    Added Animated Buttons

--- themes/default/views/earn.blade.php
+++ themes/default/views/earn.blade.php
@@ -1,15 +1,18 @@
 @extends('layouts.main')
 @section('content')
+<link href="{{ asset('css/styles.css') }}" rel="stylesheet">
 
-            <!-- AdSense auto ads code -->
-            <script data-ad-client="ca-pub-XXXXXXXXXXXXXXXX" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-            <ins class="adsbygoogle"
-                 style="display:block"
-                 data-ad-format="auto"
-                 data-full-width-responsive="true"></ins>
-            <script>
-                (adsbygoogle = window.adsbygoogle || []).push({});
-            </script>
+
+
+<!-- AdSense auto ads code -->
+<script data-ad-client="ca-pub-XXXXXXXXXXXXXXXX" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+    (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
 
 @if(!request()->is('adblocker-found'))
     @if(!request()->is('mobile'))
@@ -67,71 +70,85 @@
             detectAdblocker();
         </script>
     @endif
-    @endif
+@endif
 
-    <div class="page-content">
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="card">
-                    <div class="card-header">
-                        <div class="d-flex justify-content-between">
-                            <h5 class="card-title">{{__('Earn Coins')}}</h5>
-                        </div>
+<div class="page-content">
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="card">
+                <div class="card-header">
+                    <div class="d-flex justify-content-between">
+                        <h5 class="card-title">{{__('Earn Coins')}}</h5>
                     </div>
-                    <div class="card-body table-responsive">
+                </div>
+                <div class="card-body table-responsive">
 
-                        <table id="datatable" class="table table-striped">
-                            <thead>
-                                <tr>
-                                    <th>{{__('Type')}}</th>
-                                    <th>{{__('Reward')}}</th>
-                                    <th>{{__('Daily Limit')}}</th>
-                                    <th>{{__('Status')}}</th>
-                                    <th>{{__('Action')}}</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @if(true)
-                                <tr>
-                                    <td>Linkvertise</td>
-                                    <td><span class="badge bg-light text-dark">30 Coins</span></td>
-                                    <td><span class="badge bg-light text-dark">{{ Session::get("linkvertise_count_" . Auth::id(), 0) . "/" . app('App\Http\Controllers\EarnController')->getDailyLimit() }} Complete</span></td>
-                                    <td><span class="badge bg-success">Available</span></td>
-                                    <td><a href="{{ url('earn/lv') }}">
-                                        <Button class="btn btn-primary px-4 py-2">Start</Button>
+                    <table id="datatable" class="table table-striped">
+                        <thead>
+                        <tr>
+                            <th>{{__('Type')}}</th>
+                            <th>{{__('Reward')}}</th>
+                            <th>{{__('Daily Limit')}}</th>
+                            <th>{{__('Status')}}</th>
+                            <th>{{__('Action')}}</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @if(true)
+                            <tr>
+                                <td>Linkvertise</td>
+                                <td><span class="badge bg-light text-dark">5 Coins</span></td>
+                                <td><span class="badge bg-light text-dark">{{ Session::get("linkvertise_count_" . Auth::id(), 0) . "/" . app('App\Http\Controllers\EarnController')->getDailyLimit() }} Complete</span></td>
+                                <td><span class="badge bg-success">Available</span></td>
+                                <td>
+                                    <a href="{{ url('earn/lv') }}" class="earn-button">
+                                        <div class="animated-button">
+                                            <span></span>
+                                            <span></span>
+                                            <span></span>
+                                            <span></span>
+                                            Start
+                                        </div>
+                                    </a>
+                                </td>
+                            </tr>
+                        @endif
+
+                        @if(false)
+                            <tr>
+                                <td>Adsense</td>
+                                <td><span class="badge bg-light text-dark">5 Coins</span></td>
+                                <td><span class="badge bg-light text-dark">5 Link Per Day</span></td>
+                                <td><span class="badge bg-success">Available</span></td>
+                                <td><a href="{{ url('earn/ad') }}">
+                                        <button class="btn btn-primary px-4 py-2">Start</button>
                                     </a></td>
-                                </tr>
-                                @endif
+                            </tr>
+                        @endif
 
-                                @if(true)
-                                <tr>
-                                    <td>Adsense</td>
-                                    <td><span class="badge bg-light text-dark">5 Coins</span></td>
-                                    <td><span class="badge bg-light text-dark">Unlimited</span></td>
-                                    <td><span class="badge bg-success">Available</span></td>
-                                    <td><a href="{{ url('earn/ad') }}">
-                                        <Button class="btn btn-primary px-4 py-2">Start</Button>
+                        @if(true)
+                            <tr>
+                                <td>Clickcoin</td>
+                                <td><span class="badge bg-light text-dark">1 Coins</span></td> <!-- Replace X with the actual reward amount -->
+                                <td><span class="badge bg-light text-dark">Unlimited</span></td>
+                                <td><span class="badge bg-success">Available</span></td>
+                                <td><a href="{{ url('earn/clickcoin') }}" target="_blank">
+                                        <div class="animated-button">
+                                            <span></span>
+                                            <span></span>
+                                            <span></span>
+                                            <span></span>
+                                            Start
+                                        </div>
                                     </a></td>
-                                </tr>
-                                @endif
-
-                                @if(true)
-                                <tr>
-                                   <td>Clickcoin</td>
-                                   <td><span class="badge bg-light text-dark">1 Coins</span></td> <!-- Replace X with the actual reward amount -->
-                                   <td><span class="badge bg-light text-dark">Unlimited</span></td>
-                                   <td><span class="badge bg-success">Available</span></td>
-                                   <td><a href="{{ url('earn/clickcoin') }}" target="_blank">
-                                       <button class="btn btn-primary px-2 py-1">Start</button>
-                                     </a></td>
-                                </tr>
-                                @endif
-                            </tbody>
-                        </table>
-                    </div>
+                            </tr>
+                        @endif
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
     </div>
-@endsection
+</div>
 
+@endsection
